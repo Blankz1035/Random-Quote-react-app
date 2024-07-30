958bb4012a3dc8529fcb2c0ca492a49f74b2b74d
Blankz1035 <andrew.blankley@hotmail.com>
2024-07-30 19:18:52 +0100

    image quote api and styling for UI. modifying server to send the buffered image to UI. added more UI styling

--- randomquoteapp/src/App.js
+++ randomquoteapp/src/App.js
@@ -5,52 +5,90 @@ function App() {
   const [quote, setQuote] = useState('');
   const [showQuote, setShowQuote] = useState(false);
   const [selectedMode, setMode] = useState("random")
-  
+  const [imageSrc, setImageSrc] = useState(null);
+
   
   useEffect(() => {
-      fetchRandomQuote();
+    if (selectedMode !== "image"){
+       fetchRandomQuote(); 
+    }
+    else{
+        fetchImageQuote();
+    }
   }, [selectedMode]);
 
   const fetchRandomQuote = async () => {
-      const response = await fetch(`http://localhost:5000/quotes/${selectedMode}`);
-      const data = await response.json();
-      setQuote(data[0]);
-      setShowQuote(true);
+    try {
+        const response = await fetch(`http://localhost:5000/quotes/${selectedMode}`);
+        const data = await response.json();
+        setQuote(data[0]);
+    } catch (error) {
+        console.log(error.message);
+        setQuote("Oops. Looks like the quote didnt come back. Sorry about that. Please try again.");
+    }
+
+    setShowQuote(true);
   };
-  
+
+  const fetchImageQuote = async () => {
+    try {
+        const response = await fetch(`http://localhost:5000/quotes/${selectedMode}`);
+        const blob = await response.blob();
+        const imageUrl = URL.createObjectURL(blob);
+        setImageSrc(imageUrl);
+    } catch (error) {
+        console.log(error.message);
+        alert("Quote with image couldnt be found. Don't lose hope. Try again in a minute.")
+    }
+    
+        setShowQuote(true);
+    };
+
   const handleModeChange = (mode) => {
+    setShowQuote(false);
     setMode(mode);
   };
 
   return (
     <div className="app-container">
-            <div className="mode-container">
-                <p className="mode-text">Set your Inspiration Mode</p>
-                <div className="buttons-container">
-                    <button
-                        className={`mode-button ${selectedMode === 'random' ? 'selected' : ''}`}
-                        onClick={() => handleModeChange('random')}
-                    >
-                        <span>Random</span>
-                    </button>
-                    <button
-                        className={`mode-button ${selectedMode === 'today' ? 'selected' : ''}`}
-                        onClick={() => handleModeChange('today')}
-                    >
-                        <span>Today's Quote</span>
-                    </button>
-                    <button
-                        className={`mode-button ${selectedMode === 'image' ? 'selected' : ''}`}
-                        onClick={() => handleModeChange('image')}
-                    >
-                        <span>Image and Quote</span>
-                    </button>
-                </div>
-            </div>
-            <div className="quote-container">
-                <p className={`quote-text ${showQuote ? 'show' : ''}`}>{quote.q}</p>
+        <div className="mode-container">
+            <p className="mode-text">Set your Inspiration Mode</p>
+            <div className="buttons-container">
+                <button
+                    className={`mode-button ${selectedMode === 'random' ? 'selected' : ''}`}
+                    onClick={() => handleModeChange('random')}
+                >
+                    <span>Random</span>
+                </button>
+                <button
+                    className={`mode-button ${selectedMode === 'today' ? 'selected' : ''}`}
+                    onClick={() => handleModeChange('today')}
+                >
+                    <span>Today's Quote</span>
+                </button>
+                <button
+                    className={`mode-button ${selectedMode === 'image' ? 'selected' : ''}`}
+                    onClick={() => handleModeChange('image')}
+                >
+                    <span>Image and Quote</span>
+                </button>
             </div>
         </div>
+        <div className="quote-container">
+            {selectedMode === 'image' ? (
+                    <img
+                        className={`quote-image ${showQuote ? 'show' : 'hide'}`}
+                        src={imageSrc} // Assuming the API returns an `imageURL` property for images
+                        alt="Random Quote"
+                    />
+            ) : (
+                <p className={`quote-text ${showQuote ? 'show' : 'hide'}`}>{quote.q}</p>
+            )}
+        </div>
+        <small className='text-center'>
+            API attribution to https://zenquotes.io/
+        </small>
+    </div>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -10,15 +10,31 @@ app.use(express.json()); // able to get json data from incoming request object
 
 app.get("/quotes/:id", async(req,res) =>{
     try {
-        console.log("Starting get")
         // endpoint id is required -  should be available
         const { id } = req.params;
-        console.log(id)
-        const response = await fetch(`https://zenquotes.io/api/${id}`);
-        const data = await response.json();
-        console.log(data)
-        // q, a - h is html formatted -> send back all to react app
-        res.json(data);
+
+        if (id != "image"){
+            const response = await fetch(`https://zenquotes.io/api/${id}`);
+            const data = await response.json()
+            console.log(data)
+            // q, a - h is html formatted -> send back all to react app
+            res.json(data);
+        }
+        else{
+            const response = await fetch(`https://zenquotes.io/api/${id}`, 
+                {
+                    headers: {
+                        'Content-Type': 'image/jpeg'
+                    }
+                }
+            );
+            const contentType = response.headers.get('content-type');
+            const arrayBuffer = await response.arrayBuffer();
+            const buffer = Buffer.from(arrayBuffer);
+    
+            res.setHeader('Content-Type', contentType);
+            res.send(buffer);
+        }
     } catch (error) {
         console.log(error.message)
         res.status(500).json({ error: 'Failed to fetch quote' });
